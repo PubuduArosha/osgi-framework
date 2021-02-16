8a5c479c7d0dee40ab46097b8de479564ef487d4
Pubudu Arosha <it18108514@my.sliit.lk>
2021-02-16 20:52:34 +0530

    lab 01 completed

--- ServicePublisher/src/servicepublisher/ServiceActivator.java
+++ ServicePublisher/src/servicepublisher/ServiceActivator.java
@@ -1,30 +1,30 @@
 package servicepublisher;
 
+//package com.mtit.service;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import servicepublisher.ServicePublish;
+import servicepublisher.ServicePublishImpl;
 
 public class ServiceActivator implements BundleActivator {
 
-	private static BundleContext context;
+	ServiceRegistration publishServiceRegistration;
 
-	static BundleContext getContext() {
-		return context;
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+		System.out.println("Publisher Start");
+		ServicePublish publisherService = new ServicePublishImpl();
+		publishServiceRegistration = context.registerService(
+				ServicePublish.class.getName(), publisherService, null);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		ServiceActivator.context = bundleContext;
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Publisher Stop");
+		publishServiceRegistration.unregister();
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		ServiceActivator.context = null;
-	}
-
 }
--- ServicePublisher/src/servicepublisher/ServicePublishImpl.java
+++ ServicePublisher/src/servicepublisher/ServicePublishImpl.java
@@ -1,5 +1,9 @@
 package servicepublisher;
 
-public class ServicePublishImpl {
+public class ServicePublishImpl implements ServicePublish{
 
+	@Override
+	public String messageService(String message) {
+		return "Message by service provider execute " + message;		
+	}
 }
